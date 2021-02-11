ce73e555c7d00e446451b8c7a7df2a7589c7f1a7
= <=>
2021-02-11 18:00:07 +0530

    login form created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 20}
-    has_secure_password
     has_many :messages
+    has_secure_password
 end
