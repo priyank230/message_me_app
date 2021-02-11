6fc7dbae3a0d70f5f8cc21029213bb98d6ea4c00
= <=>
2021-02-11 18:18:06 +0530

    User login and logout routes added and session management done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
     def new
     end
+    
+    def create
+        user = User.find_by(username: params[:session][:password])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id 
+            flash[:success] = "Welcome #{user.username}!"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Credentials were not correct"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out!"
+        redirect_to login_path
+    end
 end
