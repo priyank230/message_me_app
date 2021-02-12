6e96178d4914b13434ecc736f7c40d1b4cf7b9c0
= <=>
2021-02-12 10:51:20 +0530

    Adding messages from UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,7 @@
 
 $(document).on('turbolinks:load', () => {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade')
+    })
 })
